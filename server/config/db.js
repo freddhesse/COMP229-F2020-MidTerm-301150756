026045da6a14680da17665322bb96733bf95f728
freddhesse <fredd.hesse@gmail.com>
2020-10-30 21:21:00 -0400

    Fixing db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,1 @@
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://fhesse:GmbMniVoPzrmGgz3@comp229books.fe9nw.mongodb.net/books?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-  
+module.exports.URL = "mongodb+srv://fhesse:GmbMniVoPzrmGgz3@comp229books.fe9nw.mongodb.net/Books?retryWrites=true&w=majority";
