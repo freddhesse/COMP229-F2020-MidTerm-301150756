2c29f550ac8fb0655e18c61dc08d8e7a8c53863a
freddhesse <fredd.hesse@gmail.com>
2020-10-30 21:15:40 -0400

    Populated DB Collection with bookinsert.js / Tried to fix db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,9 @@
-module.exports.URL = "mongodb+srv://fhesse:<adminpowers>@comp229books.fe9nw.mongodb.net/books?retryWrites=true&w=majority";
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://fhesse:GmbMniVoPzrmGgz3@comp229books.fe9nw.mongodb.net/books?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+  
